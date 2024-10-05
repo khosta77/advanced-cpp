ffe1a8237e0a52542d793fc43e51f63ba3b0fd0b
khosta77 <puwerfulpants@gmail.com>
2024-10-05 23:07:36 +0300

    Вынес тесты в отдельные файлы

---
+++ project/test.h
@@ -0,0 +1,23 @@
+#ifndef CPP_COURSE_TEST_H_
+#define CPP_COURSE_TEST_H_
+
+#include "lru.h"
+
+#include <cassert>
+
+struct TestObj
+{
+    std::string _key;
+    int _value;
+    bool _put;
+};
+
+extern std::vector<TestObj> BaseFromLeetCode;
+extern std::vector<TestObj> DoubleKey;
+extern std::vector<TestObj> EmptyCache;
+extern std::vector<TestObj> ManyDoubleElement;
+
+bool TEST_CACHE_PUTaGET( const int& capacity, std::vector<TestObj>& testScript );
+void test();
+
+#endif  // CPP_COURSE_TEST_H_
