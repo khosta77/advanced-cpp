6ccb37c6111abb51021aea8d9e2da48715f73b11
khosta77 <puwerfulpants@gmail.com>
2024-10-05 18:39:34 +0300

    В прошлом семе, на АиСД в ДЗ2 было задание 1. Взял его, но надо допилить

--- project/main.cpp
+++ project/main.cpp
@@ -5,6 +5,148 @@
 #include <cassert>
 #include <exception>
 
+template <typename T = std::string>
+size_t hashStr( const T& key, const size_t& random_state, const size_t& maximum = 1e9 )
+{
+    size_t hash = 0, pow = 1;
+    for ( auto element : key ) 
+    {
+        hash = ( hash + element * pow ) % maximum;
+        pow = ( pow * random_state ) % maximum;
+    }
+    return hash;
+}
+
+template <typename T = std::string>
+size_t getDoubleHash(const std::string& key )
+{
+    return ( ( hashStr( key, 41 ) << 32 ) | hashStr( key, 42 ) );
+}
+
+#define CELL_EMPTY  0x00
+#define CELL_DELETE 0x01
+#define CELL_KEY    0x02
+
+template <typename T = std::string>
+struct Cell
+{
+    T _item;
+    size_t _hash;
+    uint8_t _state;
+
+    Cell() : _hash( 0 ), _state( CELL_EMPTY ) {}
+    Cell( T item, const size_t& hash ) : _item( std::move( item ) ), _hash( hash ),
+                                         _state( CELL_KEY ) {}
+};
+
+
+template <typename T = std::string>
+class HashTable
+{
+    size_t _size;
+    size_t ( *_hsh )( const T& );
+    std::vector<Cell<T>> _table;
+
+    void reBuild()
+    {
+        std::vector<Cell<T>> buffer( ( _table.size() * 2 ) ); 
+        size_t hash = 0, cnt = 0;
+        for( size_t i  = 0; i < _table.size(); ++i )
+        {
+            if( _table[i]._state == CELL_KEY )
+            {
+                hash = ( _table[i]._hash % buffer.size() );
+                while( cnt = 0, buffer[hash]._state != CELL_EMPTY )
+                    hash = ( ( hash + ++cnt ) % buffer.size() );
+                buffer[hash] = _table[i];
+            }
+        }
+        _table = std::move( buffer );
+        buffer.clear();
+    }
+
+    /** @brief universalHashTableMethod - операции добавления, удаления и поиска элемента +- одинаковые 
+     *                                    по реализации. Решил что лучше будет объединить в один метод.
+     *                                    Чтобы не было большого повторяющегося кода. появлется небольшое
+     *                                    ветвление, но оно выполняется за О(1), так что им можно принебречь.
+     *  @param item - вводимое значение
+     *  @param isAdded - если мы хотим добавить элемент true
+     *  @param isDeleted - если мы хотим удалить true
+     *  +-------+---------+-----------+
+     *  |isAdded|isDeleted| Результат |
+     *  +-------+---------+-----------+
+     *  | fasle | false   | Поиск     |
+     *  +-------+---------+-----------+
+     *  | true  | fasle   | Добавление|
+     *  +-------+---------+-----------+
+     *  | fasle | true    | Удаление  |
+     *  +-------+---------+-----------+
+     *  | true  | true    | Ошибка!!! |
+     *  +-------+---------+-----------+
+     * */
+    bool universalHashTableMethod( const T& item, const bool& isAdded, const bool& isDeleted )
+    {
+        // Так как у метода сложность O(1), то вызов его еще раз сделает тогда сложность O(2) -> O(1)
+        if( isAdded && universalHashTableMethod( item, false, false ) )
+            return false;
+
+        if( isAdded && ( ( float )_size >= ( 0.75 * ( float )_table.size() ) ) )
+            reBuild();
+
+        const size_t hashValue = _hsh( item );
+        size_t hash = ( hashValue % _table.size() ), cnt = 0;
+        // удаленный элемент из ряда идущих элементов
+        std::pair<bool, size_t> deletedItem = std::pair<bool, size_t>( true, 0 );
+
+        while( ( _table[hash]._state != CELL_EMPTY ) && ( cnt < _table.size() ) )
+        {
+            if( ( _table[hash]._item == item ) && ( _table[hash]._state != CELL_DELETE) )
+            {
+                if( isDeleted )
+                    _table[hash]._state = CELL_DELETE;
+                return ( ( !isAdded ) ? true : false);
+            }
+
+            if( isAdded && ( ( _table[hash]._state == CELL_DELETE ) && ( deletedItem.first ) ) )
+            {
+                deletedItem = std::pair<bool, size_t>( false, hash );
+                break;  // Если бы попали на удаленную ячейку, сразу можно выйти
+            }
+
+            hash = ( ( ++hash ) % _table.size() );
+            ++cnt;
+        }
+
+        if( !isAdded )  // Если мы не добавлеям новый элемент, то возвращаем false
+            return false;
+
+        hash = ( ( deletedItem.first ) ? hash : deletedItem.second );
+        _table[hash] = Cell( std::move( item ), hashValue );
+        ++_size;
+        return true;
+    }
+
+public:
+    HashTable( size_t ( *_hasher )( const T& ) = getDoubleHash ) : _hsh(_hasher), _size(0)
+        { _table.resize(8); }
+
+    ~HashTable() { _table.clear(); }
+
+    bool find( const T& item ) { return universalHashTableMethod( item, false, false ); }
+
+    bool add( const T& item ) { return universalHashTableMethod( item, true, false ); }    
+    
+    bool remove( const T& item ) { return universalHashTableMethod( item, false, true ); }
+
+    void print()
+    {
+        std::cout << std::endl;
+        for( auto cell : _table )
+            std::cout << "h " << cell._hash << " e " << cell._item << " s " << ((int)cell._state) << std::endl;
+    }
+
+};
+
 /** @class LRUCacheException - родительский класс ошибок для всего LRUcache, выводит сообщение с ошибкой
  * */
 class LRUCacheException : public std::exception
