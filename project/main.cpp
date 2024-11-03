fc3c2943e14f03ef35abbf5d37bf776fc347a7b2
khosta77 <puwerfulpants@gmail.com>
2024-11-03 16:48:38 +0300

    Вынес тесты LRU в отдельный namespace и вызвал их в main

--- project/main.cpp
+++ project/main.cpp
@@ -4,6 +4,7 @@ int main( int argc, char* argv[] )
 {
     HashTestSpace::test();
     ListTestSpace::test();
+    LRUTestSpace::test();
     return 0;
 }
 
--- project/test.h
+++ project/test.h
@@ -562,109 +562,109 @@ namespace HashTestSpace
 
 };  // HashTestSpace
 
-struct TestObj
+namespace LRUTestSpace
 {
-    std::string _key;
-    int _value;
-    bool _put;
-};
-
-std::vector<TestObj> BaseFromLeetCode = {
-    { "1", 1, true },
-    { "2", 2, true },
-    { "1", 1, false },
-    { "3", 3, true },
-    { "2", -1, false },
-    { "4", 4, true },
-    { "1", -1, false },
-    { "3", 3, false },
-    { "4", 4, false }
-};
-
-std::vector<TestObj> DoubleKey = {
-    { "A", 1, true },
-    { "A", 1, false },
-    { "A", 1, false },
-    { "B", 2, true },
-    { "A", 3, true },
-    { "A", 3, false}
-};
-
-std::vector<TestObj> EmptyCache = {
-    { "A", -1, false }
-};
-
-std::vector<TestObj> ManyDoubleElement = {
-    { "A", 1, true },
-    { "A", 1, true },
-    { "A", 1, true },
-    { "A", 1, true },
-    { "A", 1, true },
-    { "A", 1, true },
-    { "A", 1, false }
-};
-
-
-bool TEST_CACHE_PUTaGET( const int& capacity, std::vector<TestObj>& testScript )
-{
-    LRUCache<std::string, int> cache(capacity);
-    int value = 0;
-    for( const TestObj& testObj : testScript )
+    struct TestObj
     {
-        if( testObj._put )
+        std::string _key;
+        int _value;
+        bool _put;
+    };
+
+    std::vector<TestObj> BaseFromLeetCode = {
+        { "1", 1, true },
+        { "2", 2, true },
+        { "1", 1, false },
+        { "3", 3, true },
+        { "2", -1, false },
+        { "4", 4, true },
+        { "1", -1, false },
+        { "3", 3, false },
+        { "4", 4, false }
+    };
+
+    std::vector<TestObj> DoubleKey = {
+        { "A", 1, true },
+        { "A", 1, false },
+        { "A", 1, false },
+        { "B", 2, true },
+        { "A", 3, true },
+        { "A", 3, false}
+    };
+
+    std::vector<TestObj> EmptyCache = {
+        { "A", -1, false }
+    };
+
+    std::vector<TestObj> ManyDoubleElement = {
+        { "A", 1, true },
+        { "A", 1, true },
+        { "A", 1, true },
+        { "A", 1, true },
+        { "A", 1, true },
+        { "A", 1, true },
+        { "A", 1, false }
+    };
+
+    bool TEST_CACHE_PUTaGET( const int& capacity, std::vector<TestObj>& testScript )
+    {
+        LRUCache<std::string, int> cache(capacity);
+        int value = 0;
+        for( const TestObj& testObj : testScript )
         {
-            try
+            if( testObj._put )
             {
-                cache.insert( testObj._key, testObj._value );
-            }
-            catch( const LRUCCapacityEquallNull& capNull )
-            {
-                if( capacity == 0 )
-                    continue;
-                return false;
-            }
-            catch( const std::exception& emsg )
-            {
-                return false;
-            };
-        }
-        else
-        {
-            try
-            {
-                value = cache.at( testObj._key );
-            }
-            catch( const LRUCCapacityEquallNull& capNull )  // Если размер изначальный равен 0
-            {
-                if( capacity == 0 )
-                    continue;
-                return false;
-            }
-            catch( const std::exception& emsg )
-            {
-                if( testObj._value != -1 )
+                try
                 {
+                    cache.insert( testObj._key, testObj._value );
+                }
+                catch( const LRUCCapacityEquallNull& capNull )
+                {
+                    if( capacity == 0 )
+                        continue;
                     return false;
                 }
-                continue;
-            };
-
-            if( value != testObj._value )
+                catch( const std::exception& emsg )
+                {
+                    return false;
+                };
+            }
+            else
             {
-                return false;
+                try
+                {
+                    value = cache.at( testObj._key );
+                }
+                catch( const LRUCCapacityEquallNull& capNull )  // Если размер изначальный равен 0
+                {
+                    if( capacity == 0 )
+                        continue;
+                    return false;
+                }
+                catch( const std::exception& emsg )
+                {
+                    if( testObj._value != -1 )
+                    {
+                        return false;
+                    }
+                    continue;
+                };
+
+                if( value != testObj._value )
+                    return false;
             }
         }
+        return true;
     }
-    return true;
-}
 
-void test()
-{
-    assert( TEST_CACHE_PUTaGET( 2, BaseFromLeetCode ) );
-    assert( TEST_CACHE_PUTaGET( 3, DoubleKey ) );
-    assert( TEST_CACHE_PUTaGET( 2, EmptyCache ) );
-    assert( TEST_CACHE_PUTaGET( 0, BaseFromLeetCode ) );
-    assert( TEST_CACHE_PUTaGET( 2, ManyDoubleElement ) );
-}
+    void test()
+    {
+        assert( TEST_CACHE_PUTaGET( 2, BaseFromLeetCode ) );
+        assert( TEST_CACHE_PUTaGET( 3, DoubleKey ) );
+        assert( TEST_CACHE_PUTaGET( 2, EmptyCache ) );
+        assert( TEST_CACHE_PUTaGET( 0, BaseFromLeetCode ) );
+        assert( TEST_CACHE_PUTaGET( 2, ManyDoubleElement ) );
+    }
+};  // LRUTestSpace
 
 #endif  // CPP_COURSE_TEST_H_
