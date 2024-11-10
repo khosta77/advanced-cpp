6d9ca633cb72fbbeb4c90220a2f3167c92ef5c53
khosta77 <puwerfulpants@gmail.com>
2024-11-10 22:31:49 +0300

    Перенес ошибки

--- project/LRUCache.h
+++ project/LRUCache.h
@@ -4,8 +4,39 @@
 #include <iostream>
 #include <list>
 #include <map>
+#include <string>
+#include <exception>
 
-#include "exception.h"
+/** @class LRUCacheException - родительский класс ошибок для всего LRUcache, выводит сообщение с ошибкой
+ * */
+class LRUCacheException : public std::exception
+{
+public:
+    explicit LRUCacheException( const std::string& msg ) : m_msg(msg) {}
+    const char *what() const noexcept override
+    {
+        return m_msg.c_str();
+    }
+private:
+    std::string m_msg;
+};
+
+/** @class LRUCCapacityEquallNull - класс ошибки, если capacity == 0
+ * */
+class LRUCCapacityEquallNull : public LRUCacheException
+{
+public:
+    LRUCCapacityEquallNull() : LRUCacheException( "capacity is null" ) {}
+};
+
+/** @class LRUCKeyNotFind класс ошибки если ключ не найден
+ * */
+class LRUCKeyNotFind : public LRUCacheException
+{
+public:
+    LRUCKeyNotFind() : LRUCacheException( "key not find" ) {}
+};
+
 #include "HashTable.h"
 #include "List.h"
 
