ebccb4162f7df0d36c9cdcbbf24168fadb3bd1ad
khosta77 <puwerfulpants@gmail.com>
2024-11-10 22:28:57 +0300

    Вызов новых тестов + закомитил вызов loop

--- project/main.cpp
+++ project/main.cpp
@@ -5,6 +5,8 @@ int main( int argc, char* argv[] )
     ListTestSpace::test();
     HashTestSpace::test();
     LRUTestSpace::test();
+	LRUHWTestSpace::test();
+	//loop( std::cin, std::cout );
     return 0;
 }
 
