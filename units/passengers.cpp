59ec0e21ccc234693e74ed536085a256c4c6e410
khosta77 <puwerfulpants@gmail.com>
2024-11-13 20:47:04 +0300

    Тесты. В crew_members и passengers у меня конструктор, там и тестить нечего. В plane очень много всего за раз затрагивает

--- units/passengers.cpp
+++ units/passengers.cpp
@@ -1,8 +1,33 @@
 #include <cassert>
-#include ".hpp"
+#include "passengers.hpp"
+
+void test()
+{
+    std::array<Bag, 4> bags_first = { Bag(60, 20), Bag(60, 20), Bag(200, 20), Bag(100, 20) };
+    std::array<Bag, 4> bags_business = { Bag(24, 12), Bag(24, 12), Bag(40, 12), Bag(40, 12) };
+    std::array<Bag, 4> bags_econom = { Bag(10, 13), Bag(0, 12), Bag(20, 13), Bag(0, 13) };
+
+    Passenger *first_class01 = new FirstClass( 20, bags_first );
+    Passenger *first_class02 = new FirstClass( 12, bags_business );
+    Passenger *first_class03 = new FirstClass( 13, bags_econom );
+
+    assert( first_class01->name == "FIRST_CLASS" );
+    assert( first_class02->name == "FIRST_CLASS" );
+    assert( first_class03->name == "FIRST_CLASS" );
+
+    assert( first_class01->WillWeBeAbleToRegisterIt() );
+    assert( first_class02->WillWeBeAbleToRegisterIt() );
+    assert( first_class03->WillWeBeAbleToRegisterIt() );
+	
+	delete first_class01;
+	delete first_class02;
+	delete first_class03;
+	// У остальных так же
+}
 
 int main()
 {
+	test();
     return 0;
 }
 
--- units/plane.cpp
+++ units/plane.cpp
@@ -1,8 +1,48 @@
 #include <cassert>
-#include ".hpp"
+#include <iostream>
+#include "plane.hpp"
+
+void test()
+{
+	Plane plane;
+	std::stringstream input, output, true_output;
+    input << "FIRST_CLASS_SEGMENT 400" << std::endl;
+    input << "BUSINESS_CLASS_SEGMENT 800" << std::endl;
+    input << "ECONOMY_CLASS_SEGMENT 1000" << std::endl;
+    input << "PILOT 0 0" << std::endl;
+    input << "FLIGHT_ATTENDANT 0 0" << std::endl;
+    input << "ECONOMY 5 20" << std::endl;
+    input << "ECONOMY 5 4" << std::endl;
+    input << "ECONOMY 5 10" << std::endl;
+    input << "ECONOMY 5 15" << std::endl;
+    input << "ECONOMY 5 16" << std::endl;
+    input << "ECONOMY 5 10" << std::endl;
+    input << "ECONOMY 5 20" << std::endl;
+    input << "ECONOMY 5 21" << std::endl;
+    input << "ECONOMY 5 20" << std::endl;
+    input << "ECONOMY 5 15" << std::endl;
+    input << "ECONOMY 5 6" << std::endl;
+    input << "ECONOMY 5 8" << std::endl;
+    input << "ECONOMY 5 10" << std::endl;
+    input << "BUSINESS 0 10 34" << std::endl;
+    input << "FIRST_CLASS 30 30 200 200" << std::endl;
+    input << "FIRST_CLASS 0 0 850 100" << std::endl;
+	plane.load( input, output );
+	true_output << "!!PASSENGER’S LUGGAGE REMOVED FROM FLIGHT, ID = 12!!\n";
+	true_output << "!!PASSENGER’S LUGGAGE REMOVED FROM FLIGHT, ID = 6!!\n";
+	true_output << "!!PASSENGER’S LUGGAGE REMOVED FROM FLIGHT, ID = 11!!\n";
+	true_output << "!!PASSENGER’S LUGGAGE REMOVED FROM FLIGHT, ID = 9!!\n";
+	true_output << "!!PASSENGER’S LUGGAGE REMOVED FROM FLIGHT, ID = 3!!\n";
+	true_output << "!!PASSENGER’S LUGGAGE REMOVED FROM FLIGHT, ID = 10!!\n";
+	true_output << "!!PASSENGER’S LUGGAGE REMOVED FROM FLIGHT, ID = 7!!\n";
+
+	assert(output.str() == true_output.str());
+
+}
 
 int main()
 {
-    return 0;
+	test();
+	return 0;
 }
 
