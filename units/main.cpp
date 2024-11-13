de50b7bf24d5092f7e4bb3b2642bbd9277f37e95
khosta77 <puwerfulpants@gmail.com>
2024-11-13 21:15:19 +0300

    Вызов для тестов на капах

--- units/main.cpp
+++ units/main.cpp
@@ -3,28 +3,6 @@
 int main()
 {
     Plane plane;
-    std::stringstream input;
-    input << "FIRST_CLASS_SEGMENT 400" << std::endl;
-    input << "BUSINESS_CLASS_SEGMENT 800" << std::endl;
-    input << "ECONOMY_CLASS_SEGMENT 1000" << std::endl;
-    input << "PILOT 0 0" << std::endl;
-    input << "FLIGHT_ATTENDANT 0 0" << std::endl;
-    input << "ECONOMY 5 20" << std::endl;
-    input << "ECONOMY 5 4" << std::endl;
-    input << "ECONOMY 5 10" << std::endl;
-    input << "ECONOMY 5 15" << std::endl;
-    input << "ECONOMY 5 16" << std::endl;
-    input << "ECONOMY 5 10" << std::endl;
-    input << "ECONOMY 5 20" << std::endl;
-    input << "ECONOMY 5 21" << std::endl;
-    input << "ECONOMY 5 20" << std::endl;
-    input << "ECONOMY 5 15" << std::endl;
-    input << "ECONOMY 5 6" << std::endl;
-    input << "ECONOMY 5 8" << std::endl;
-    input << "ECONOMY 5 10" << std::endl;
-    input << "BUSINESS 0 10 34" << std::endl;
-    input << "FIRST_CLASS 30 30 200 200" << std::endl;
-    input << "FIRST_CLASS 0 0 850 100" << std::endl;
-    return plane.load( input, std::cout );
+    return plane.load( std::cin, std::cout );
 }
 
