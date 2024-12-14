e79d77ac26a32f3c811c44118bc635012acfa7dc
khosta77 <puwerfulpants@gmail.com>
2024-12-15 02:50:15 +0300

    Возвращем json

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 #include "json/single_include/nlohmann/json.hpp"
+using Json = nlohmann::json;
 
 #include <boost/fusion/adapted/struct/adapt_struct.hpp>
 #include <boost/fusion/include/define_struct.hpp>
@@ -25,20 +26,27 @@ BOOST_FUSION_DEFINE_STRUCT(
     (std::string, some_str)
 )
 
-int main()
+template<typename FusionT>
+std::string Serialize( const FusionT& fusion_obj )
 {
-    std::cout << boost::fusion::extension::struct_size<pkg::Ranks>::value << std::endl;
-    pkg::Ranks ranks = {1, 2, 3, "example"};
+    Json json_obj;
+
     boost::fusion::for_each(
         boost::mpl::range_c<unsigned, 0, boost::fusion::result_of::size<pkg::Ranks>::value>(), 
         [&](auto index)
         {
-            std::cout << "Name: "
-                      << boost::fusion::extension::struct_member_name<pkg::Ranks, index>::call()
-                      << " Value: "
-                      << boost::fusion::at_c<index>(ranks) << std::endl;
+            json_obj[boost::fusion::extension::struct_member_name<pkg::Ranks, index>::call()] = 
+                boost::fusion::at_c<index>(fusion_obj);
         }
     );
 
+    return json_obj.dump();
+}
+
+int main()
+{
+    pkg::Ranks ranks = {1, 2, 3, "example"};
+    std::string json_str = Serialize(ranks);
+    std::cout << "Serialized JSON: " << json_str << std::endl;
     return 0;
 }
