861c7e503bea4861f0bb404672ea666e08b65372
khosta77 <puwerfulpants@gmail.com>
2024-12-16 17:32:17 +0300

    Удалил пробелы...

--- judge.hpp
+++ judge.hpp
@@ -22,31 +22,31 @@ using Json = nlohmann::json;
 #include <boost/mpl/vector.hpp>
 #include <boost/type_index.hpp>
 
-template <typename T>
+template<typename T>
 using is_fusion_struct = std::is_same<typename boost::fusion::traits::tag_of<T>::type, boost::fusion::struct_tag>;
 
-template <typename T>
+template<typename T>
 struct is_valid_t : std::false_type {};
 
-template <>
+template<>
 struct is_valid_t<int> : std::true_type {};
 
-template <>
+template<>
 struct is_valid_t<float> : std::true_type {};
 
-template <>
+template<>
 struct is_valid_t<std::string> : std::true_type {};
 
-template <typename T>
+template<typename T>
 struct is_valid_ts : std::disjunction<is_valid_t<T>, is_fusion_struct<T>> {};
 
-template <typename T>
+template<typename T>
 struct is_valid_vector_t : std::false_type {};
 
-template <typename T>
+template<typename T>
 struct is_valid_vector_t<std::vector<T>> : is_valid_ts<T> {};
 
-template <typename T>
+template<typename T>
 struct is_valid : std::disjunction<is_valid_ts<T>, is_valid_vector_t<T>> {};
 
 #endif  // _HW3_JUDGE_HPP_
